f276c5308fce89c9f88c359cf80a8a5e7db27f93
tkcdesign <register@tkc-design.com>
2018-12-22 10:59:01 +0900

    aaa

--- sokuwari-bot/index.js
+++ sokuwari-bot/index.js
@@ -19,20 +19,94 @@ server.post('/webhook', line.middleware(line_config), (req, res, next) => {
     res.sendStatus(200);
     //イベント処理のプロミスを格納
     let events_processed = [];
+    /*
+    function isPrice(inputPrice) {
+        //文字列最後の円を取り除く
+
+        //var price  = inputPrice.slice( 0, -1) ;
+        parseInt(inputPrice);
+        if(Number.isInteger(inputPrice)) {
+            return false;
+        }
+        return true;
+    };
+    */
+    /**
+     * ユーザーが入力した値が金額か判定
+     * @param {string} input　入力された文字列 
+     */
+    function isPrice(input) {
+        var price = input.slice( -1 ) ;
+        if(price == "円") {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * ユーザーが入力した値が人数か判定
+     * @param {string} input 入力された文字列
+     */
+    function isNumberPeople(input) {
+        var numberPeople = input.slice( -1 );
+        if(price == "人") {
+            return true;
+        }
+        return false;
+    }
     //イベントオブジェクトの処理を書く
     req.body.events.forEach((event) => {
         if (event.type == "message" && event.message.type == "text"){
-            let message_text;
+            let message_text = [];
+             var price = 0;
             if (event.message.text == "割り勘"){
-                message_text = "それでは金額を計算します。";
-            } else {
-                message_text = "aaaaa";
-            }
-
-                events_processed.push(bot.replyMessage(event.replyToken, {
+                message_text = 
+                {
                     type: "text",
-                    text: message_text
-                }));
+                    text: "それでは割り勘金額を計算します！"
+                },
+                {
+                    tpye: "text",
+                    text: "割り勘する金額を教えて下さい！ 例:1000円"
+                };
+            } else if (Number.isInteger(parseInt(event.message.text))) {
+                if(isPrice()){
+                    message_text = 
+                    {
+                        type: "text",
+                        text: "ありがとうございます。金額は" + event.message.text +"ですね！"
+                    },
+                    {
+                        type: "text",
+                        text: "次に人数を教えて下さい！"
+                    };
+                } else if(isNumberPeople()) {
+                    message_text = 
+                    {
+                        type: "text",
+                        text: "ありがとうございます。" + event.message.text + "だと一人当たりの金額は" + price +"です！"
+                    };
+                } else {
+                    message_text = 
+                    {
+                        type: "text",
+                        text: "入力に誤りがあります。数字に単位がついていない、数字が全角になってしまっているか等の原因が考えられます。"
+                    }
+                };
+            };
+            /*elseif(isPrice(event.message.text) && ) {
+                message_text = 
+                {
+                    type: "text",
+                    text: "ありがとうございます！金額は" + event.message.text + "ですね。"
+                },
+                {
+                    type: "text",
+                    text: "次に人数を入力して下さい！"
+                };
+            }
+            */
+            events_processed.push(bot.replyMessage(event.replyToken, message_text));
             }
     });
 
