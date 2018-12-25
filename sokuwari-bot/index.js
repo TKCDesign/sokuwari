8da665dabd7d7f00b65819798f784a0550df5325
tkcdesign <register@tkc-design.com>
2018-12-25 15:50:16 +0900

    aaa

--- sokuwari-bot/index.js
+++ sokuwari-bot/index.js
@@ -144,7 +144,7 @@ server.post('/webhook', line.middleware(line_config), (req, res, next) => {
                                         }],  
                                 "thumbnailImageUrl": "https://raw.githubusercontent.com/TKCDesign/sokuwari/master/sokuwari-bot/thumb.jpg",
                                 "title": "一人あたりの金額は"+ result + "円です！",             
-                                "text": "合計金額" + price + "で" + event.message.text + "の場合"
+                                "text": "合計金額" + price + "円で" + event.message.text + "の場合"
                             }
                         };
                     }
